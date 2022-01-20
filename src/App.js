e69c3ea79d49a2e8b814a916b21b02c2d22e5832
jean <eparis@atlassian.com>
2022-01-20 10:39:44 -0500

    feat: implement interfaz to calculate return

--- src/App.js
+++ src/App.js
@@ -4,45 +4,38 @@ import { calculateReturn } from "./helpers/calculateReturn";
 import Button from "./components/Button";
 
 function App() {
-  // const gasStations = [1, 2, 3, 4, 5];
-  // const costs = [3, 4, 5, 1, 2];
-  // const initialIndex = 2;
+  const [stations, setStations] = useState([]);
+  const [costs, setCosts] = useState([]);
+  const [initialIndex, setInitialIndex] = useState(0);
+  const [calculatedReturn, setCalculatedReturn] = useState(0);
+  const [submitted, setSubmitted] = useState(false);
+  const [arrayLength, setArrayLength] = useState(0);
 
-  // let currentTank = 0;
-  // let response = initialIndex;
-
-  // const stationsCosts = gasStations.map((el, index) => [el, costs[index]]);
-  // const stationsCostsOrdered = [
-  //   ...stationsCosts.slice(initialIndex),
-  //   ...stationsCosts.slice(0, initialIndex),
-  // ];
-  // stationsCostsOrdered.forEach((el) => {
-  //   currentTank = currentTank + el[0];
-  //   if (currentTank >= el[1]) {
-  //     currentTank = currentTank - el[1];
-  //   } else {
-  //     response = -1;
-  //     return;
-  //   }
-  // });
-
-  // let arrayLength = 5;
-  const [array1, setArray1] = useState([]);
-  const [array2, setArray2] = useState([]);
-  // const [initialIndex, setInitialIndex] = useState(0);
-
-  const onSetArrayLength = (el) => {
-    setArray1(Array(Number(el)).fill(""));
-    setArray2(Array(Number(el)).fill(""));
+  const onChangeArrayLength = (el) => {
+    if (submitted) setSubmitted(false);
+    setArrayLength(el);
+    setStations(Array(Number(el)).fill(null));
+    setCosts(Array(Number(el)).fill(null));
   };
-  const updateArray1 = (el, index) => {
-    let arrayTemp = array1;
-    arrayTemp[index] = el;
-    setArray1(arrayTemp);
+  const onChangeStation = (el, index) => {
+    let arrayTemp = stations;
+    arrayTemp[index] = Number(el);
+    setStations(arrayTemp);
+  };
+  const onChangeCost = (el, index) => {
+    let arrayTemp = costs;
+    arrayTemp[index] = Number(el);
+    setCosts(arrayTemp);
+  };
+  const onChangeInitialIndex = (el) => {
+    if (submitted) setSubmitted(false);
+    setInitialIndex(Number(el.target.value));
   };
   const submit = () => {
-    calculateReturn();
+    setSubmitted(true);
+    setCalculatedReturn(calculateReturn(stations, costs, initialIndex));
   };
+
   return (
     <div className="h-screen w-screen flex items-center justify-center">
       <div className="App-header">
@@ -52,56 +45,72 @@ function App() {
           </label>
           <input
             type="text"
-            onChange={(el) => onSetArrayLength(el.target.value)}
+            onChange={(el) => onChangeArrayLength(el.target.value)}
             className="border w-full rounded-md py-2 px-4 mt-1"
           />
         </div>
-        <div className="mt-5">
-          <label className="text-gray-800 block">
-            ingrese la disponibilidad de los grifos:
-          </label>
-          <div className="mt-1">
-            {array1.map((el, index) => (
+        {!!arrayLength && (
+          <div>
+            <div className="mt-5">
+              <label className="text-gray-800 block">
+                ingrese la disponibilidad de los grifos:
+              </label>
+              <div className="mt-1">
+                {stations.map((el, index) => (
+                  <input
+                    key={index}
+                    type="text"
+                    onChange={(el) => onChangeStation(el.target.value, index)}
+                    className="border w-11 rounded-md py-2 px-4 m-1"
+                  />
+                ))}
+              </div>
+            </div>
+            <div className="mt-5">
+              <label className="text-gray-800 block">
+                ingrese el costo para ir al siguiente grifo:
+              </label>
+              <div className="mt-1">
+                {costs.map((el, index) => (
+                  <input
+                    key={index}
+                    type="text"
+                    onChange={(el) => onChangeCost(el.target.value, index)}
+                    className="border w-11 rounded-md py-2 px-4 m-1"
+                  />
+                ))}
+              </div>
+            </div>
+            <div className="mt-5">
+              <label className="text-gray-800 block">
+                ingrese el grifo donde comenzara:
+              </label>
               <input
-                key={index}
                 type="text"
-                onChange={(el) => updateArray1(el.target.value, index)}
-                className="border w-11 rounded-md py-2 px-4 m-1"
+                onChange={(el) => onChangeInitialIndex(el)}
+                className="border w-full rounded-md py-2 px-4 mt-1"
               />
-            ))}
+            </div>
+            <Button
+              form="primary"
+              type="submit"
+              styles="mt-5 w-full flex justify-center items-center"
+              onClick={submit}
+            >
+              Calcular
+            </Button>
           </div>
-        </div>
-        <div className="mt-5">
-          <label className="text-gray-800 block">
-            ingrese el costo para ir al siguiente grifo:
-          </label>
-          <div className="mt-1">
-            {array2.map((el, index) => (
-              <input
-                key={index}
-                type="text"
-                className="border w-11 rounded-md py-2 px-4 m-1"
-              />
-            ))}
-          </div>
-        </div>
-        <div className="mt-5">
-          <label className="text-gray-800 block">
-            ingrese el index inicial:
-          </label>
-          <input
-            type="text"
-            onChange={(el) => onSetArrayLength(el.target.value)}
-            className="border w-full rounded-md py-2 px-4 mt-1"
-          />
-        </div>
-        <Button
-          form="primary"
-          type="submit"
-          styles="mt-5 w-full flex justify-center items-center"
-        >
-          Calcular
-        </Button>
+        )}
+        {submitted && (
+          <p
+            className={`mt-5 ${
+              calculatedReturn === -1 ? "text-red-500" : "text-teal-500"
+            }`}
+          >
+            Su gasolina {calculatedReturn === -1 && "no"} sera suficiente para regresar
+            al grifo {initialIndex}
+          </p>
+        )}
       </div>
     </div>
   );
