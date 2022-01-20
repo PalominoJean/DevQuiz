2ee1244bd444341369ccdb1e20e48c7891fb613f
jean <eparis@atlassian.com>
2022-01-20 18:08:34 -0500

    feat: add validation inputs in App

--- src/App.js
+++ src/App.js
@@ -2,113 +2,140 @@ import "./App.css";
 import { useState } from "react";
 import { calculateReturn } from "./helpers/calculateReturn";
 import Button from "./components/Button";
+import { useForm } from "react-hook-form";
 
 function App() {
-  const [stations, setStations] = useState([]);
-  const [costs, setCosts] = useState([]);
-  const [initialIndex, setInitialIndex] = useState(0);
-  const [calculatedReturn, setCalculatedReturn] = useState(0);
-  const [submitted, setSubmitted] = useState(false);
-  const [arrayLength, setArrayLength] = useState(0);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({});
 
-  const onChangeArrayLength = (el) => {
-    if (submitted) setSubmitted(false);
-    setArrayLength(el);
-    setStations(Array(Number(el)).fill(null));
-    setCosts(Array(Number(el)).fill(null));
-  };
-  const onChangeStation = (el, index) => {
-    let arrayTemp = stations;
-    arrayTemp[index] = Number(el);
-    setStations(arrayTemp);
-  };
-  const onChangeCost = (el, index) => {
-    let arrayTemp = costs;
-    arrayTemp[index] = Number(el);
-    setCosts(arrayTemp);
-  };
+  const [initialIndex, setInitialIndex] = useState(0);
+  const [calculatedReturn, setCalculatedReturn] = useState(null);
+  const [errorSizeArray, setErrorSizeArray] = useState(false);
+
   const onChangeInitialIndex = (el) => {
-    if (submitted) setSubmitted(false);
-    setInitialIndex(Number(el.target.value));
+    setCalculatedReturn(null);
+    setInitialIndex(Number(el));
   };
-  const submit = () => {
-    setSubmitted(true);
-    setCalculatedReturn(calculateReturn(stations, costs, initialIndex));
+  const submit = (form) => {
+    let _stations = form.stations.split(",");
+    let _costs = form.costs.split(",");
+
+    if (_stations.length === _costs.length) {
+      setErrorSizeArray(false);
+      setCalculatedReturn(calculateReturn(_stations, _costs, initialIndex));
+    } else {
+      setErrorSizeArray(true);
+    }
   };
 
   return (
     <div className="h-screen w-screen flex items-center justify-center">
       <div className="App-header">
-        <div>
-          <label className="text-gray-800 block">
-            ingrese el tamanio del array:
-          </label>
-          <input
-            type="text"
-            onChange={(el) => onChangeArrayLength(el.target.value)}
-            className="border w-full rounded-md py-2 px-4 mt-1"
-          />
-        </div>
-        {!!arrayLength && (
-          <div>
-            <div className="mt-5">
-              <label className="text-gray-800 block">
-                ingrese la disponibilidad de los grifos:
-              </label>
-              <div className="mt-1">
-                {stations.map((el, index) => (
-                  <input
-                    key={index}
-                    type="text"
-                    onChange={(el) => onChangeStation(el.target.value, index)}
-                    className="border w-11 rounded-md py-2 px-4 m-1"
-                  />
-                ))}
-              </div>
-            </div>
-            <div className="mt-5">
-              <label className="text-gray-800 block">
-                ingrese el costo para ir al siguiente grifo:
-              </label>
-              <div className="mt-1">
-                {costs.map((el, index) => (
-                  <input
-                    key={index}
-                    type="text"
-                    onChange={(el) => onChangeCost(el.target.value, index)}
-                    className="border w-11 rounded-md py-2 px-4 m-1"
-                  />
-                ))}
-              </div>
-            </div>
-            <div className="mt-5">
-              <label className="text-gray-800 block">
-                ingrese el grifo donde comenzara:
-              </label>
+        <form onSubmit={handleSubmit(submit)}>
+          <div className="mt-5">
+            <label className="text-gray-800 block">
+              ingrese los grifos(separado por comas):
+            </label>
+            <div className="mt-1">
               <input
                 type="text"
-                onChange={(el) => onChangeInitialIndex(el)}
-                className="border w-full rounded-md py-2 px-4 mt-1"
+                className={`border w-full rounded-md py-2 px-4 m-1 ${
+                  errors.stations?.message
+                    ? "border-red-500"
+                    : "border-gray-200"
+                }`}
+                {...register(`stations`, {
+                  required: "Por favor ingrese como minimo 1 grifo",
+                  pattern: {
+                    value: /^(\d+,)+\d+$/,
+                    message: "Ingrese un array valido",
+                  },
+                })}
               />
+              {errors.stations && (
+                <p className="text-sm font-medium mt-1 text-red-500">
+                  {errors.stations.message}
+                </p>
+              )}
             </div>
-            <Button
-              form="primary"
-              type="submit"
-              styles="mt-5 w-full flex justify-center items-center"
-              onClick={submit}
-            >
-              Calcular
-            </Button>
           </div>
-        )}
-        {submitted && (
+          <div className="mt-5">
+            <label className="text-gray-800 block">
+              ingrese los costos(separado por comas):
+            </label>
+            <div className="mt-1">
+              <input
+                type="text"
+                className={`border w-full rounded-md py-2 px-4 m-1 ${
+                  errors.costs?.message ? "border-red-500" : "border-gray-200"
+                }`}
+                {...register(`costs`, {
+                  required: "Por favor ingrese como minimo 1 costo",
+                  pattern: {
+                    value: /^(\d+,)+\d+$/,
+                    message: "Ingrese un array valido",
+                  },
+                })}
+              />
+              {errors.costs && (
+                <p className="text-sm font-medium mt-1 text-red-500">
+                  {errors.costs.message}
+                </p>
+              )}
+            </div>
+          </div>
+          <div className="mt-5">
+            <label className="text-gray-800 block">
+              ingrese el grifo donde comenzara:
+            </label>
+            <input
+              type="text"
+              className={`border w-full rounded-md py-2 px-4 m-1 ${
+                errors.initialIndex?.message
+                  ? "border-red-500"
+                  : "border-gray-200"
+              }`}
+              {...register(`initialIndex`, {
+                required: "Por favor ingrese el grifo donde iniciara.",
+                pattern: {
+                  value: /^[0-9]\d*$/,
+                  message: "Por favor ingrese un numero valido",
+                },
+              })}
+              onChange={(e) => onChangeInitialIndex(e.target.value)}
+            />
+            {errors.initialIndex && (
+              <p className="text-sm font-medium mt-1 text-red-500">
+                {errors.initialIndex.message}
+              </p>
+            )}
+          </div>
+          {errorSizeArray && (
+            <p className="text-sm font-medium mt-1 text-red-500">
+              los arrays de grifos y costos deben ser del mismo tamanio
+            </p>
+          )}
+
+          <Button
+            form="primary"
+            type="submit"
+            styles="mt-5 w-full flex justify-center items-center"
+          >
+            Calcular
+          </Button>
+        </form>
+
+        {calculatedReturn !== null && (
           <p
             className={`mt-5 ${
               calculatedReturn === -1 ? "text-red-500" : "text-teal-500"
             }`}
           >
-            Su gasolina {calculatedReturn === -1 && "no"} sera suficiente para regresar
-            al grifo {initialIndex}
+            Su gasolina {calculatedReturn === -1 && "no"} sera suficiente para
+            regresar al grifo {initialIndex}
           </p>
         )}
       </div>
--- src/helpers/calculateReturn.js
+++ src/helpers/calculateReturn.js
@@ -2,7 +2,10 @@ export const calculateReturn = (gasStations, costs, initialIndex) => {
   let currentTank = 0;
   let response = initialIndex;
 
-  const stationsCosts = gasStations.map((el, index) => [el, costs[index]]);
+  const stationsCosts = gasStations.map((el, index) => [
+    Number(el),
+    costs[index],
+  ]);
   const stationsCostsOrdered = [
     ...stationsCosts.slice(initialIndex),
     ...stationsCosts.slice(0, initialIndex),
